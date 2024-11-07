5ac4efbd090e06e3f8f6e2bc5031cc462adb20b2
Pratham2712 <prathamvaishya123@gmail.com>
2024-11-07 17:22:42 +0530

    stage wise data

--- src/pages/StageLoading/StageLoading.jsx
+++ src/pages/StageLoading/StageLoading.jsx
@@ -11,21 +11,85 @@ import "swiper/css/thumbs";
 import "./StageLoading.css";
 
 import { FreeMode, Navigation, Thumbs, Pagination, Zoom } from "swiper/modules";
+import { useLocation } from "react-router-dom";
+import { rgbaToHex } from "../../Util/util";
+
+const heading = ["Name", "Length", "Width", "Height", "Filled", "Total"];
 
 const StageLoading = () => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const [imgData, setImgData] = useState({});
-  console.log(imgData);
+  const [showSku, setShowSku] = useState([]);
+  const [imgColors, setImgColors] = useState([]);
+  const location = useLocation();
+  const { inputData, filled, boxData } = location.state || {};
+
+  console.log(inputData, filled);
+
+  const replaceHex = (colorCode) => {
+    colorCode = colorCode.replace("0x", "#");
+    return colorCode;
+  };
+
+  const generateData = (imgData) => {
+    console.log(imgData);
+    Object.keys(imgData).forEach((ele) => {
+      if (ele > 0) {
+        for (let i = 0; i < imgData?.[ele]?.color?.length - 1; i++) {
+          console.log(imgData[ele].color[i], ele);
+          const currColor = imgData[ele].color[i];
+
+          for (let j = 0; j < parseInt(inputData.numTypes); j++) {
+            const colorKey = `color${j}`;
+
+            if (rgbaToHex(inputData[colorKey]) == replaceHex(currColor)) {
+              const data = {
+                filled: boxData[j],
+                totalCases: inputData[`numberOfCases${j}`],
+                name: inputData[`sku${j}`],
+                length: inputData[`length${j}`],
+                width: inputData[`width${j}`],
+                height: inputData[`height${j}`],
+                rotationAllowed: inputData[`rotationAllowed${j}`],
+              };
+
+              setShowSku((prev) => {
+                const alreadyExists = prev.some((item) => item[currColor]);
+                if (alreadyExists) return prev; // If currColor exists, return current state without adding
+                return [...prev, { [currColor]: data }]; // Else, add new entry
+              });
+            }
+          }
+        }
+      }
+    });
+  };
+  console.log(showSku);
 
   useEffect(() => {
     const data = JSON.parse(localStorage.getItem("boxData")) || {};
     const lastImg = localStorage.getItem("screenshot");
     setImgData(data);
+    const color = [];
+    console.log(data);
+    color.push(...data?.[Object.keys(data)?.length - 1]?.color);
+    color.push(data?.[Object.keys(data)?.length - 1]?.color?.[0]);
+    console.log("color", color);
+
     setImgData((prev) => ({
       ...prev,
-      [Object.keys(prev).length]: { screenshot: lastImg },
+      [Object.keys(prev).length]: { screenshot: lastImg, color: color },
     }));
   }, []);
+
+  console.log(imgData);
+
+  useEffect(() => {
+    if (Object.keys(imgData).length > 0) {
+      generateData(imgData);
+    }
+    setImgColors(imgData?.[1]?.color);
+  }, [imgData]);
 
   return (
     <>
@@ -47,14 +111,18 @@ const StageLoading = () => {
           thumbs={{ swiper: thumbsSwiper }}
           modules={[FreeMode, Navigation, Thumbs, Pagination, Zoom]}
           className="mySwiper"
+          onSlideChange={(swiper) => {
+            setImgColors(imgData[swiper.activeIndex + 1]?.color);
+          }}
         >
           {Object.keys(imgData)?.map((ele) => {
-            console.log(imgData[ele], "ele", imgData);
-            return (
-              <SwiperSlide>
-                <img src={imgData[ele]?.screenshot} />
-              </SwiperSlide>
-            );
+            if (imgData[ele]?.screenshot) {
+              return (
+                <SwiperSlide>
+                  <img src={imgData[ele]?.screenshot} />
+                </SwiperSlide>
+              );
+            }
           })}
         </Swiper>
         <Swiper
@@ -67,13 +135,56 @@ const StageLoading = () => {
           className="mySwiper2"
         >
           {Object.keys(imgData)?.map((ele) => {
-            return (
-              <SwiperSlide>
-                <img src={imgData[ele]?.screenshot} />
-              </SwiperSlide>
-            );
+            if (imgData[ele]?.screenshot) {
+              return (
+                <SwiperSlide>
+                  <img src={imgData[ele]?.screenshot} />
+                </SwiperSlide>
+              );
+            }
           })}
         </Swiper>
+      </div>
+      <div
+        className="sku-details"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: "0.5rem",
+          width: "100%",
+          justifyContent: "center",
+        }}
+      >
+        <table>
+          <tr>
+            {heading.map((ele) => {
+              return <th style={{ padding: "8px" }}>{ele}</th>;
+            })}
+          </tr>
+          {imgColors?.map((item, index) => {
+            if (
+              index < imgColors?.length - 1 &&
+              index < parseInt(inputData?.["numTypes"])
+            ) {
+              const sku = showSku.find((obj) => obj[item]);
+
+              return (
+                <>
+                  <tr>
+                    <td style={{ background: replaceHex(item) }}>
+                      {sku[item]?.name}
+                    </td>
+                    <td>{sku[item]?.length}</td>
+                    <td>{sku[item]?.width}</td>
+                    <td>{sku[item]?.height}</td>
+                    <td>{sku[item]?.filled}</td>
+                    <td>{sku[item]?.totalCases}</td>
+                  </tr>
+                </>
+              );
+            }
+          })}
+        </table>
       </div>
     </>
   );
